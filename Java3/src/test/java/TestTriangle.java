440c2514618f1ce07f2472faa162c7d6b00dc58b
sazonovamaria <sazonovamaria1@gmail.com>
2021-08-29 18:57:19 +0300

    ДЗ4

---
+++ Java3/src/test/java/TestTriangle.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+public class TestTriangle {
+    @Test
+    Function functions = new Function();
+
+    void testGivenTriangle() {
+        double result = functions.triangleArea();
+        Assertions.assertTrue(result);
+    }
+
+
+}
